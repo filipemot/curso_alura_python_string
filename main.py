38647bb5d506c5765dc5076dea2203da8a4f6c51
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-05-05 17:23:06 -0300

    Utilizando métodos de String

---
+++ main.py
@@ -0,0 +1,20 @@
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+
+#Separa base e parametro
+indice_interrogacao = url.find('?')
+url_base = url[0:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+#Busca o valor de um parametro
+parametro_busca = "moedaDestino"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if(indice_e_comercial == -1):
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
+
